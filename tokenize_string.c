5adeec4a619971a96879f9f4b37c697894814435
Greentaylor27 <7196@holbertonstudents.com>
2024-03-24 11:42:43 -0500

    Tokenize string function

---
+++ tokenize_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* tokenize_string - tokenizes string, stores strings at array indeces
+*
+* @str: string to be tokenized
+* @delims: delimiter to tokenize by
+* @tokens: array to hold tokenized strings
+* Return: void
+*/
+void tokenize_string(char *str, char *delims, char **tokens)
+{
+    char *path_token = strtok(str, delims);
+    int i = 0;
+
+    while (path_token != NULL)
+    {
+        /* Tokenizes string */
+        tokens[i] = path_token;
+        i++;
+        path_token = strtok(NULL, delims);
+    }
+    tokens[i] = NULL;
+}
