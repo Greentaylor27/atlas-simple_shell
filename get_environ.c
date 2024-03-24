d9a1ff9b8454227a25abc1e0eb2e49030e39d0ad
Greentaylor27 <7196@holbertonstudents.com>
2024-03-24 11:40:35 -0500

    Get environ function

---
+++ get_environ.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+* *get_environ - gets the environment variable, excluding "PATH="
+*
+* @environ: environment variable, globally declared in main.h
+* Return: path, excluding "PATH="
+*/
+char *get_environ(char **environ)
+{
+    int i = 0;
+    char *path = NULL;
+
+    while (environ[i] != NULL)
+    {
+        /* Checks if environ is equal to PATH= */
+        if (strncmp(environ[i], "PATH=", 5) == 0)
+        {
+            /* Copies path starting at the sixth character in PATH= string */
+            path = strdup(environ[i] + 5);
+            if (path == NULL)
+            {
+                perror("path");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
+        i++;
+    }
+    return (path);
+}
