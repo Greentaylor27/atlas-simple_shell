d0f74f43105a7c703a83039e7adfd327a27f5d19
Tsimmuaj <tyang4204@gmail.com>
2024-07-18 12:02:24 -0500

    Added a path file

---
+++ path.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _path - returns the path of a command
+ * @string: command of the path to find
+ * Return: 1 on success and 0 on fail
+ */
+int _path(char *string)
+{
+	
+}
