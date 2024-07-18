6ff58e971b8faf1e0e89b655cc75f0731fd6370e
Tsimmuaj <tyang4204@gmail.com>
2024-07-18 12:37:45 -0500

    added a path finding function

--- path.c
+++ path.c
@@ -1,10 +1,29 @@
 #include "main.h"
 /**
  * _path - returns the path of a command
- * @string: command of the path to find
- * Return: 1 on success and 0 on fail
+ * @string: environment to find a path
+ * Return: returns path or NULL on fail
  */
-int _path(char *string)
+char *_path(char *env)
 {
-	
+	char *path = NULL;
+	int number = 0;
+
+	while (env[number] != '\0')
+		number++;
+	path = malloc(sizeof(char) * number);
+	if (path == NULL)
+	{
+		free(path);
+		return (0);
+	}
+	path = strtok(env, "\n");
+	while(strtok(NULL, "\n"))
+	{
+		path = strtok(NULL, "\n");
+		if (strcmp(path, "PATH=", 5))
+			return (path);
+	}
+	free(path);
+	return (NULL);
 }
