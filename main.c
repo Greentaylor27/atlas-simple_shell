f6d1cc003fc8c81f20ea3a7d524f840bdbd10dc3
Tsimmuaj <tyang4204@gmail.com>
2024-07-23 14:26:10 -0500

    testing execve

--- main.c
+++ main.c
@@ -5,8 +5,39 @@
  * @av: array holding arguements
  * Return: 0 on true and -1 on false
  */
-int main(void)
+int main(int ac, char **av)
 {
-	printf("%s\n", _path());
-	return (0);
+	char *string = _path(), *str;
+	struct stat *name;
+
+	if (ac < 3)
+	{
+		name = malloc(sizeof(struct stat));
+		if (name == NULL)
+		{
+			free(name);
+			return (-1);
+		}
+		str = malloc(sizeof(char) * 1024);
+		if (str == NULL)
+		{
+			free(name);
+			free(str);
+			return (-1);
+		}
+		str = strtok(string, ":");
+		while (str != NULL)
+		{
+			if (stat(str, name) == 0)
+			{
+				str = strcat(str, "/");
+				execve(str, av, environ);
+				break;
+			}
+			str = strtok(NULL, ":");
+		}
+		printf("Didn't work\n");
+	}
+	printf("Nothing happend\n");
+	return (1);
 }
